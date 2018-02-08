286e7c473ee43470cb699c5a0592282227ca7cf9
Vilhjalmr12 <guillermomm1230@gmail.com>
2018-02-08 12:12:12 +0100

    GI finished

--- CodeChat/app/src/main/java/uu/pss_group/f/codechat/SignUpActivity.java
+++ CodeChat/app/src/main/java/uu/pss_group/f/codechat/SignUpActivity.java
@@ -1,7 +1,6 @@
 package uu.pss_group.f.codechat;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,68 +15,68 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     //Attributes
-    private Button loginButton, signupButton;
-    private EditText mailField, passwordField;
+    private Button signupButton;
+    private EditText mailField, passwordField, passwordConfField;
     private ProgressDialog loadingD;
     private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_signup);
 
-        loginButton = findViewById(R.id.login_loginButton);
-        signupButton = findViewById(R.id.login_signupButton);
-        mailField = findViewById(R.id.login_emailField);
-        passwordField = findViewById(R.id.login_passwordField);
+        signupButton = (Button) findViewById(R.id.signup_signupButton);
+        mailField = (EditText) findViewById(R.id.signup_emailField);
+        passwordField = (EditText) findViewById(R.id.signup_passwordField);
+        passwordConfField = (EditText) findViewById(R.id.signup_passwordConfField);
         loadingD = new ProgressDialog(this);
-        loginButton.setOnClickListener(this);
         signupButton.setOnClickListener(this);
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onClick(View view) {
-        if (view == loginButton) {
-            logInUser();
-        } else if (view == signupButton) {
-            Intent myIntent = new Intent(this, SignUpActivity.class);
-            startActivity(myIntent);
+        if (view == signupButton) {
+            SignUpUser();
         }
     }
 
-    private void logInUser() {
+    private String passwordCheck(String psw){
+        if(     !psw.matches(".*[A-Z].*") ||
+                !psw.matches(".*[a-z].*") ||
+                !psw.matches(".*\\d.*")   ||
+                psw.length() < 10) return null;
+        return psw;
+    }
+
+    private void SignUpUser() {
         String email = mailField.getText().toString().trim();
-        String password = passwordField.getText().toString().trim();
+        String password = passwordCheck(passwordField.getText().toString().trim());
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Please fill in the Email field", Toast.LENGTH_LONG).show();
             return;
         }
         if (TextUtils.isEmpty(password) || password == null) {
-            Toast.makeText(getApplicationContext(), "Please fill in the Password field", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Password should contain at least one uppercase"+
+                    "character, a lowercase character, a digit and should be 10 characters long!", Toast.LENGTH_LONG).show();
             return;
         }
-        loadingD.setMessage("Loging In...");
+        loadingD.setMessage("Registering User...");
         loadingD.show();
-        firebaseAuth.signInWithEmailAndPassword(email, password)
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            startMainMenu();
+                            Toast.makeText(getApplicationContext(), "User successfully signed up", Toast.LENGTH_LONG).show();
                         } else {
                             Toast.makeText(getApplicationContext(), "Something went wrong... Please try again", Toast.LENGTH_LONG).show();
                         }
                     }
-                });
+        });
         loadingD.cancel();
-    }
-
-    private void startMainMenu() {
-        Intent myIntent = new Intent(this, MainActivity.class);
-        startActivity(myIntent);
     }
 }
